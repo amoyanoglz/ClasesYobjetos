35bff3f7fd1ab39f62827e6c468f9a86549068f1
Carlos Mestre <carmesce@gmail.com>
2023-06-12 11:44:31 +0200

    Main reestructurado, múltiples aulas y alumnos

--- main.py
+++ main.py
@@ -2,20 +2,62 @@ from alumno import Alumno
 from aula import Aula
 from profesor import Profesor
 
-mi_aula = Aula()
-mi_profe = Profesor("Juanito Balerrama", 0, 10)
+CANT_AULAS = 5
 
-mi_aula.set_profesor(mi_profe)
-mi_aula.add(Alumno("Pepito", "A", "pepito@a.com"))
-mi_aula.add(Alumno("Lucia", "A", "lucia@a.com"))
-mi_aula.add(Alumno("Martita", "A", "martita@b.com"))
-mi_aula.add(Alumno("Gloria", "A", "gloria@b.com"))
-mi_aula.add(Alumno("Juanito", "A", "juanito@c.com"))
-mi_aula.add(Alumno("Jorgito", "A", "jorgito@c.com"))
-mi_aula.add(Alumno("Jaimito", "A", "jaimito@c.com"))
+aulas = []
+for i in range(CANT_AULAS):
+    aulas.append(Aula())
 
-mi_aula.puntuar()
-mi_aula.listar()
-print()
-print("CONVOCADOS:")
-mi_aula.convocar_examen("A")
+profesores = [
+    Profesor("Juanito Balerrama", 0, 10),
+    Profesor("Manolito Pomporretas", 0, 10),
+    Profesor("Teres Rabal", 0, 10),
+]
+
+alumnos = [
+    Alumno("Pepito", "A", "pepito@a.com"),
+    Alumno("Lucia", "A", "lucia@a.com"),
+    Alumno("Martita", "A", "martita@b.com"),
+    Alumno("Juanito", "A", "juanito@c.com"),
+    Alumno("Jorgito", "A", "jorgito@c.com"),
+    Alumno("Gloria", "A", "gloria@b.com"),
+    Alumno("Jaimito", "A", "jaimito@c.com"),
+    Alumno("Pepo", "A", "Pepo@c.com"),
+    Alumno("Alberta", "A", "Alberta@c.com"),
+    Alumno("Canada", "A", "Canada@c.com"),
+    Alumno("Adam", "A", "Adam@c.com"),
+    Alumno("Rosita", "A", "Rosita@c.com"),
+    Alumno("Juana", "A", "Juana@c.com"),
+    Alumno("Macarena", "A", "Macarena@c.com"),
+    Alumno("Rigoberto", "A", "Rigobert@c.com"),
+    Alumno("YYYYYYYYto", "A", "YYYYYYYY@c.com"),
+    Alumno("Cristiano", "A", "Cristiano@c.com")
+]
+
+aulas[0].set_profesor(profesores[0])
+aulas[1].set_profesor(profesores[1])
+aulas[2].set_profesor(profesores[2])
+aulas[4].set_profesor(profesores[0])
+
+contador = 0
+while contador < len(alumnos):
+    aulas[contador % (len(aulas) - 1)].add(alumnos[contador])
+    contador += 1
+
+contador = 0
+for aula in aulas:
+    print(f"AULA {contador} ----------------------------- ")
+
+    try:
+        aula.puntuar()
+    except Exception as e:
+        print(e)
+
+    try:
+        aula.convocar_examen("A")
+    except Exception as e:
+        aula.listar()
+        print(e)
+
+    print("\n" * 3)
+    contador += 1
