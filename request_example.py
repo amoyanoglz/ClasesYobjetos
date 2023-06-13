9637f5f5a3dcf576eb1cf5066f2a3021de3e648b
Carlos Mestre <carmesce@gmail.com>
2023-06-13 09:48:50 +0200

    Native urllib -> requests

--- request_example.py
+++ request_example.py
@@ -1,11 +1,46 @@
-import json
-import urllib.request
+import requests
+# recuerda instalar la libreria
+#     Win: pip install requests
+#     Mac: pip3 install requests
 
-# urlData = 'https://api.coinbase.com/v2/exchange-rates?currency=USD'
-urlData = 'https://randomuser.me/api/?results=' + input("Cuantos quieres? ")
+def get_json_from(uri):
+    r = requests.get(uri)
+    # print(r.status_code)
+    # print(r.headers['content-type'])
+    # print(r.encoding)
+    # print(r.text)
+    # print(r.json())
 
-webURL = urllib.request.urlopen(urlData)
-data = webURL.read()
-encoding = webURL.info().get_content_charset('utf-8')
-JSON_object = json.loads(data.decode(encoding))
-print(JSON_object)
+    return r.json()
+
+def pretty(d, indent=0):
+    for key, value in d.items():
+        print('  ' * indent + str(key))
+        if isinstance(value, dict):
+            pretty(value, indent+1)
+        else:
+            print('\t' * (indent+1) + str(value))
+
+def main():
+    results = "1"  # input("Cuantos quieres? ")
+    uri = f'https://randomuser.me/api/?results=1{results}'
+    dict = get_json_from(uri)
+
+    for user in dict["results"]:
+        pretty(user)
+
+if __name__ == "__main__":
+    main()
+
+# Viejo request: No funciona a la priemra en windows:
+
+# import json
+# import urllib.request
+
+# def get_json_from(uri):
+#     urlopenRet = urllib.request.urlopen(urlData)
+#     data = urlopenRet.read()
+#     encoding = urlopenRet.info().get_content_charset('utf-8')
+#     JSON_object = json.loads(data.decode(encoding))
+
+#     return JSON_object
