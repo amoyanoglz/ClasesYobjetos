f7272f94f9f2234ee569ee2753136ca44cfc04a6
Carlos Mestre <carmesce@gmail.com>
2023-06-13 14:24:26 +0200

    quitado print (pretty)

--- request_example.py
+++ request_example.py
@@ -5,21 +5,12 @@ def get_json_from(uri):
 
     return r.json()
 
-def pretty(d, indent=0):
-    for key, value in d.items():
-        print('  ' * indent + str(key))
-        if isinstance(value, dict):
-            pretty(value, indent+1)
-        else:
-            print('\t' * (indent+1) + str(value))
 
 def main():
     results = "1"  # input("Cuantos quieres? ")
     uri = f'https://randomuser.me/api/?results=1{results}'
     dict = get_json_from(uri)
 
-    for user in dict["results"]:
-        pretty(user)
 
 if __name__ == "__main__":
     main()
